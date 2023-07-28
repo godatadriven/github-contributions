25b6fcce12b72bf6cd4a99f916a0950f0b80f2e8
Cor Zuurmond <jczuurmond@protonmail.com>
2023-07-28 09:44:36 +0200

    Update the plugin docstrings

--- github_contributions/src/github_contributions/plugin.py
+++ github_contributions/src/github_contributions/plugin.py
@@ -11,10 +11,31 @@ from . import api as github_api
 
 class Plugin(BasePlugin):
     def initialize(self, plugin_config: dict[str, Any]) -> None:
+        """Initialize the plugin
+
+        The configuration is specfied in the profile.
+
+        Parameters
+        ----------
+        plugin_config : dict[str, Any]
+            A dictionary representing the plugin configuration
+        """
         github_token = plugin_config.get("GITHUB_TOKEN", os.getenv("GITHUB_TOKEN"))
         self.headers = github_api.create_headers(github_token)
 
     def load(self, source_config: SourceConfig) -> pd.DataFrame:
+        """Load the data for a source.
+
+        Parameters
+        ----------
+        source_config : SourceConfig
+            The configuration of the source
+
+        Returns
+        -------
+        out : pd.DataFrame
+            The public pull requests
+        """
         author = source_config.name
         df = github_api.search_author_public_pull_requests(
             author, headers=self.headers
