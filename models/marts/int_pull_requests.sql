bb5849288a14b3e4ef9f066645e827fc7fceaad0
Cor Zuurmond <jczuurmond@protonmail.com>
2023-07-28 14:33:12 +0200

    Extract owner and repository

--- models/marts/int_pull_requests.sql
+++ models/marts/int_pull_requests.sql
@@ -1,8 +1,23 @@
+WITH extract_owner_and_repository AS (
+
+SELECT
+    *,
+    regexp_extract(
+        html_url,
+        '^https:/\/github\.com\/(.+)\/(.+)\/pull\/\d+$',
+        ['owner', 'repository']
+    ) AS owner_and_repository
+FROM {{ ref("stg_pull_requests") }}
+
+)
+
 SELECT
     title,
     body,
     user_login AS author,
     author_association,
+    owner_and_repository.owner,
+    owner_and_repository.repository,
     state,
     draft,
     comments,
@@ -12,4 +27,4 @@ SELECT
     reactions_total_count,
     pull_request_merged_at AS merged_at,
     html_url AS url,
-FROM {{ ref("stg_pull_requests") }}
+FROM extract_owner_and_repository
