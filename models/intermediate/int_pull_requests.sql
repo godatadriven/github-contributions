fd36fc080a15cdd850a097e88c84adef1672fd0e
Cor Zuurmond <jczuurmond@protonmail.com>
2023-07-29 09:14:05 +0200

    Update regex

--- models/intermediate/int_pull_requests.sql
+++ models/intermediate/int_pull_requests.sql
@@ -2,7 +2,7 @@ SELECT
     *,
     regexp_extract(
         html_url,
-        '^https:/\/github\.com\/(.+)\/(.+)\/pull\/\d+$',
+        '^https:\/\/github\.com\/(.+)\/(.+)\/pull\/\d+$',
         ['owner', 'repository']
     ) AS owner_and_repository
 FROM {{ ref("stg_pull_requests") }}
