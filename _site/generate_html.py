6744f35e60d3a64a0952872260d4df893951f37a
Cor Zuurmond <jczuurmond@protonmail.com>
2023-08-02 18:43:40 +0200

    Update website generation

--- _site/generate_html.py
+++ _site/generate_html.py
@@ -1,39 +1,57 @@
+from __future__ import annotations
+
+import argparse
 from pathlib import Path
 
 import duckdb
-import pandas as pd
 from jinja2 import Environment, FileSystemLoader
 
+
 TEMPLATES_PATH = Path(__file__).parent / "templates"
-INDEX_TEMPLATE = "index.html"
+PULL_REQUESTS_TEMPLATE = "pull_requests.html"
 
 
-def main():
-    # Load data from github_contributions.duckdb into pandas dataframe
-    connection = duckdb.connect(database="github_contributions.duckdb")
+def generate_html(duckdb_database: str | Path, output_file: str | Path) -> None:
+    """Generate the HTML page that summarizes Xebia's Github contributions.
 
-    pull_requests_sql = "SELECT * FROM main_consumers_xebia.consm_xebia_pull_requests"
-    pull_requests = pd.read_sql_query(pull_requests_sql, connection)
+    Parameters
+    ----------
+    duckdb_database : str | Path
+        The path to the Duckdb database
+    output_directory : str | Path
+        The directory to output the webpage into
+    """
+    connection = duckdb.connect(str(duckdb_database), read_only=True)
 
-    # Load some pull request info for use in the html
-    pull_requests["username"] = pull_requests["user"].apply(lambda x: x["login"])
-    unique_contributors = pull_requests["username"].unique().tolist()
-    pull_requests = pull_requests[["title", "html_url"]].drop_duplicates().to_records(index=False)
+    pull_requests_sql = (
+        "SELECT author, title, url "
+        "FROM main_consumers_xebia.consm_xebia_pull_requests"
+    )
+    pull_requests = connection.sql(pull_requests_sql).df()
 
-    # generate html using jinja2
+    unique_authors = pull_requests["author"].unique().tolist()
+    titles_and_urls = pull_requests[["title", "url"]].drop_duplicates().to_records(index=False)
+
     env = Environment(loader=FileSystemLoader(TEMPLATES_PATH))
-    template = env.get_template(INDEX_TEMPLATE)
+    template = env.get_template(PULL_REQUESTS_TEMPLATE)
     html = template.render(
-        contributors=unique_contributors,
-        pull_requests=pull_requests,
+        authors=unique_authors,
+        titles_and_urls=titles_and_urls,
     )
 
-    # Write html to file
-    path = Path("public")
-    path.mkdir(exist_ok=True)
-    with (path / INDEX_TEMPLATE).open("w") as f:
+    with Path(output_file).open("w") as f:
         f.write(html)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser("Generate Xebia pull request website")
+
+    parser.add_argument("duckdb_database", type=Path)
+    parser.add_argument("output_file", type=Path)
+
+    args = parser.parse_args()
+
+    if not args.duckdb_database.is_file():
+        raise ValueError(f"Database does not exists: {args.duckdb_database}")
+
+    generate_html(args.duckdb_database, args.output_file)
