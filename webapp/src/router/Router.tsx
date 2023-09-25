c017fee5feca8ec9ec20dc8089d08ed53ef3b9dc
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2023-09-25 09:55:06 +0200

    Update Router.tsx

--- webapp/src/router/Router.tsx
+++ webapp/src/router/Router.tsx
@@ -1,7 +1,9 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import routes from '../router/routes.tsx';
 
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, {
+    basename: '/github-contributions'
+});
 
 function Router() {
     return <RouterProvider router={router}/>;
