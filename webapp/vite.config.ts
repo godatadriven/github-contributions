397c7305d024337b1c7a5bd9fe580dab0619ec80
Ramon Vermeulen <ramonvermeulen98@gmail.com>
2023-09-25 10:51:16 +0200

    Update vite.config.ts

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -5,4 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   assetsInclude: ['src/assets/github_contributions.duckdb'],
   plugins: [react()],
+  base: '/github-contributions/'
 });
